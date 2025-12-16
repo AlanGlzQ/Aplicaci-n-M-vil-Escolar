3ac07311b1a7397e9a68b170154b77c2a113add1
Alan González Quintero <156737437+AlanGlzQ@users.noreply.github.com>
2025-12-16 17:05:20 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,9 @@
 export const DIRECTORY_URL = "https://cuceimobile.space/directorio.html";
 export const AUTH_URL = "https://cuceimobile.space/campusCucei/auth.php";
 export const VIDEO_URL = "https://alanglzq.github.io/miapp-videos/";
+
 export const USE_MOCK_AUTH = true; // false en produccion
+
+// > Nota: Los valores en `config.js` están simplificados para este demo.
+// > En un entorno real debería usarse configuración con variables de entorno
+// > y manejo adecuado de credenciales.
